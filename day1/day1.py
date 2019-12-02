accb14a775a18ea04cc5d706678735bc76ef87a6
Kelsey Ando <kelsey.ando@detroitlabs.com>
2019-12-02 09:48:54 -0500

    adding input file and making it so the day1 reads the inout file and calculates the sum of the masses

--- day1/day1.py
+++ day1/day1.py
@@ -1,8 +1,25 @@
+import sys
+inFile = sys.argv[1]
+
+with open(inFile,'r') as i:
+    lines = i.readlines()
+
 def calcMass(num):
     x = lambda a : a / 3
     return x(num) - 2
 
-print(calcMass(12) == 2)
-print(calcMass(14) == 2)
-print(calcMass(1969) == 654)
-print(calcMass(100756) == 33583)
+def calcSum(array):
+    sum = 0
+    for item in array:
+        number = int(item.strip())
+        sum += calcMass(number)
+    return sum
+
+
+print(calcSum(lines))
+
+#calcMass tests
+#print(calcMass(12) == 2)
+#print(calcMass(14) == 2)
+#print(calcMass(1969) == 654)
+#print(calcMass(100756) == 33583)
