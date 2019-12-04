f241a514637dc77d589948376a1ac843506d1b06
Kelsey Ando <kelsey.ando@detroitlabs.com>
2019-12-03 21:07:40 -0500

    adding the rest of what I have including the code to get it working all you have to do is add you input

--- day2/day2.py
+++ day2/day2.py
@@ -1,15 +1,26 @@
 import sys
 
-line = open(sys.argv[1],'r').read()
 
 def getInput(string):
     parsedStr = string.strip().split(",")
     return map(int, parsedStr)
 
+def replaceData(array):
+    array[1] = 12
+    array[2] = 2
+    return array
+
 
 def compute(data):
-    #input = getInput(data)
-    input = data
+    if type(data) == str:
+        input = getInput(data)
+        print("BEFORE")
+        print(input)
+        input = replaceData(input)
+        print("AFTER")
+        print(input)
+    else:
+        input = data
     i = 0
     getValue = lambda a : input[input[a]]
     while input[i] != 99:
@@ -32,6 +43,25 @@ line = [2,3,0,3,99]
 test2 = compute(line)
 print(test2 == [2,3,0,6,99])
 print(test2)
+
+## TEST 3 multiplication
+line = [2,4,4,5,99,0]
+test3 = compute(line)
+print(test3 == [2,4,4,5,99,9801])
+print(test3)
+
+## TEST 4 multiplication
+line = [1,1,1,4,99,5,6,0,99]
+test4 = compute(line)
+print(test4 == [30,1,1,4,2,5,6,0,99])
+print(test4)
+
+## Actual
+line = open(sys.argv[1],'r').read()
+real = compute(line)
+print(compute(line))
+
+
 
 
 
